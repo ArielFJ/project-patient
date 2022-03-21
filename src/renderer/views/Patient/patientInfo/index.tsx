8bfdaf174912d5455a0832e98f1b71100c1c9e51
Jesús Ariel Fermín <afermin266@gmail.com>
2022-03-20 20:34:28 -0400

    ref: Add Edit Consultation button

--- src/renderer/views/Patient/patientInfo/index.tsx
+++ src/renderer/views/Patient/patientInfo/index.tsx
@@ -10,7 +10,7 @@ import { Patient } from 'shared/database/entities/Patient';
 import { useNavigate, useParams } from 'react-router';
 import { useAppDispatch } from 'renderer/store/hooks';
 import { updatePatientAsync } from 'renderer/store/patients/asyncThunks';
-import { IconChevronLeft, IconPlus } from '@tabler/icons';
+import { IconChevronLeft, IconPencil, IconPlus } from '@tabler/icons';
 import AnimateButton from 'renderer/ui-component/extended/AnimateButton';
 import PatientForm from '../components/PatientForm';
 import Channels from 'shared/ipcChannels';
@@ -18,6 +18,7 @@ import DialogContainer, { DialogContainerRef } from 'renderer/ui-component/Dialo
 import ConsultationForm from 'renderer/views/Consultation/components/ConsultationForm';
 import { DataGrid, GridColDef, GridRowParams, GridValueGetterParams } from '@mui/x-data-grid';
 import { Consultation } from 'shared/database/entities/Consultation';
+import FloatingButton from 'renderer/ui-component/FloatingButton';
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
@@ -59,7 +60,7 @@ const PatientInfoPage: React.FC = (): JSX.Element => {
     // Parse GridRowParams properties into an object of type Consultation
     setSelectedConsultation({ ...Object.assign(new Consultation(), params.row), patient });
     setIsUpdatingConsultation(true);
-    dialogContainerRef.current?.Open();
+    // dialogContainerRef.current?.Open();
   };
 
   const onPatientUpdateSubmitted = (patientToUpdate: Patient) => {
@@ -168,15 +169,16 @@ const PatientInfoPage: React.FC = (): JSX.Element => {
                 rows={consultations ?? []}
                 columns={columns}
                 rowHeight={100}
+                onRowClick={onEditConsultationClicked}
                 getRowClassName={getRowClassName}
-                onRowDoubleClick={onEditConsultationClicked}
+                onRowDoubleClick={() => dialogContainerRef.current?.Open()}
               />
             </Box>
           </>
         )}
       </MainCard>
 
-      <DialogContainer title={isUpdatingConsultation ? "Update consultation" :"New Consultation"} ref={dialogContainerRef}>
+      <DialogContainer title={isUpdatingConsultation ? 'Update consultation' : 'New Consultation'} ref={dialogContainerRef}>
         <ConsultationForm
           patient={patient}
           onSubmit={onNewConsultationSubmitted}
@@ -184,6 +186,19 @@ const PatientInfoPage: React.FC = (): JSX.Element => {
           isUpdating={isUpdatingConsultation}
         />
       </DialogContainer>
+
+      <FloatingButton
+        title="Edit Patient"
+        onClick={() => dialogContainerRef.current?.Open()}
+        childContent={<IconPencil />}
+        styles={{
+          backgroundColor: '#2196f3',
+          display: selectedConsultation ? 'block' : 'none',
+          '&: hover': {
+            backgroundColor: '#1e88e5'
+          }
+        }}
+      />
     </>
   );
 };
